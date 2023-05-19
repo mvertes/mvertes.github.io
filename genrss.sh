080f966a82460153f65ea8785ba9e7d63b17ae8c
Marc Vertes <mvertes@free.fr>
2023-05-19 23:53:36 +0200

    revert

--- genrss.sh
+++ genrss.sh
@@ -17,8 +17,7 @@ fixhtml() {
 . ./meta.sh
 cat <<- EOT
 	<?xml version="1.0" encoding="UTF-8"?>
-	<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-	<atom:link href="$link/feed.xml" rel="self" type="application/rss+xml" />
+	<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
 	<channel>
 	<title>$title</title>
 	<link>$link/</link>
